0ad16d3de9800b79cdf1e4bef5e03ba575a76c0a
Ali Loubani <alil@microtech.solutions>
2020-09-10 21:41:56 +0300

    Formatting code

--- Assets/Scripts/BecomeDriver.cs
+++ Assets/Scripts/BecomeDriver.cs
@@ -8,28 +8,37 @@ using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BecomeDriver : Panel {
+public class BecomeDriver : Panel
+{
     public Driver driver = null;
     public Text title;
-    public Button becomeDriver, editRegions;
+    public Button becomeDriver,
+    editRegions;
     public ListView listView;
     public List<RegionItem> regionItems = new List<RegionItem>();
     private List<Location> regions = new List<Location>();
     public static int regionCounter = 0;
-    public void submit() {
-        if (Validate()) {
+    public void submit()
+    {
+        if (Validate())
+        {
             regions.Clear();
-            foreach (var item in regionItems) {
+            foreach (var item in regionItems)
+            {
                 regions.Add(item.getRegion());
             }
             driver = new Driver(regions);
             AddCarPanel p = PanelsFactory.CreateAddCar();
-            Open(p, () => { p.Init(driver); });
+            Open(p, () => {
+                p.Init(driver);
+            });
         }
     }
 
-    public void EditRegions() {
-        if (Validate()) {
+    public void EditRegions()
+    {
+        if (Validate())
+        {
             foreach (var item in regionItems)
             {
                 regions.Add(item.getRegion());
@@ -40,10 +49,11 @@ public class BecomeDriver : Panel {
             request.AddSendListener(OpenSpinner);
             request.AddReceiveListener(CloseSpinner);
             request.Send(EditResponse);
-            
+
         }
     }
-    private void EditResponse(Driver driver, int code, string message) {
+    private void EditResponse(Driver driver, int code, string message)
+    {
         if (!code.Equals((int)HttpStatusCode.OK))
         {
             OpenDialog(message, false);
@@ -55,21 +65,29 @@ public class BecomeDriver : Panel {
             MissionCompleted(ProfilePanel.PANELNAME, "Regions Updated", true);
         }
     }
-    public void AddItemToList() {
-        if (regionCounter <= 2) {
+    public void AddItemToList()
+    {
+        if (regionCounter <= 2)
+        {
             var obj = ItemsFactory.CreateRegionItem(listView.scrollContainer, this);
             listView.Add(obj.gameObject);
             regionItems.Add(obj);
             regionCounter += 1;
-            if (regionCounter == 1) {
+            if (regionCounter == 1)
+            {
                 regionItems[0].deleteButton.gameObject.SetActive(false);
-            } else {
+            }
+            else
+            {
                 regionItems[0].deleteButton.gameObject.SetActive(true);
             }
-        } else OpenDialog("You have added the maximum number of regions", false);
+        }
+        else OpenDialog("You have added the maximum number of regions", false);
     }
-    public void AddItemToList(List<Location> regions) {
-        foreach (var region in regions) {
+    public void AddItemToList(List<Location> regions)
+    {
+        foreach (var region in regions)
+        {
             var obj = ItemsFactory.CreateRegionItem(listView.scrollContainer, region, this);
             listView.Add(obj.gameObject);
             regionItems.Add(obj);
@@ -77,15 +95,19 @@ public class BecomeDriver : Panel {
             ValidateDeleteRegion();
         }
     }
-    public void AddRegion() {
+    public void AddRegion()
+    {
 
-        if (ValidateAddingRegions()) {
+        if (ValidateAddingRegions())
+        {
             AddItemToList();
         }
     }
-    public override void Init() {
+    public override void Init()
+    {
 
-        if (Status==StatusE.ADD) {
+        if (Status == StatusE.ADD)
+        {
             Clear();
             AddItemToList();
             title.text = "Become A Driver";
@@ -93,7 +115,8 @@ public class BecomeDriver : Panel {
             becomeDriver.gameObject.SetActive(true);
 
         }
-        else if (Status == StatusE.VIEW) {
+        else if (Status == StatusE.VIEW)
+        {
             Clear();
             AddItemToList(Program.Driver.regions);
             title.text = "Regions";
@@ -103,58 +126,71 @@ public class BecomeDriver : Panel {
 
     }
 
-
-    internal override void Clear() {
+    internal override void Clear()
+    {
         listView.Clear();
         regionCounter = 0;
         editRegions.gameObject.SetActive(false);
         becomeDriver.gameObject.SetActive(true);
         title.text = "Become a driver";
     }
-    public bool Validate() {
+    public bool Validate()
+    {
         bool valid = true;
-        for (int i = 0; i < regionItems.Count; i++) {
-            if (!regionItems[i].Validate()) {
+        for (int i = 0; i < regionItems.Count; i++)
+        {
+            if (!regionItems[i].Validate())
+            {
                 OpenDialog("Insert the region then click next", false);
                 valid = false;
             }
 
         }
-        if (regionItems.Count == 4) {
+        if (regionItems.Count == 4)
+        {
             OpenDialog("You have Add The Maximum Number Of Regions", false);
             valid = false;
 
         }
 
-
         return valid;
     }
-    public bool ValidateAddingRegions() {
+    public bool ValidateAddingRegions()
+    {
         bool valid = true;
         print(regionItems.Count + " regionItems");
-        for (int i = 0; i < regionItems.Count; i++) {
-            if (!regionItems[i].Validate()) {
+        for (int i = 0; i < regionItems.Count; i++)
+        {
+            if (!regionItems[i].Validate())
+            {
                 OpenDialog("Please add the previous region first", false);
                 valid = false;
             }
         }
         return valid;
     }
-    public bool ValidateRegionName(string region) {
+    public bool ValidateRegionName(string region)
+    {
         bool valid = true;
-        for (int i = 0; i < regionItems.Count; i++) {
-            if (regionItems[i].ToString().Equals(region)) {
+        for (int i = 0; i < regionItems.Count; i++)
+        {
+            if (regionItems[i].ToString().Equals(region))
+            {
                 OpenDialog("This region is already exist", false);
                 valid = false;
             }
         }
         return valid;
     }
-    public void ValidateDeleteRegion() {
-        for (int i = 0; i < regionCounter; i++) {
-            if (i > 0) {
+    public void ValidateDeleteRegion()
+    {
+        for (int i = 0; i < regionCounter; i++)
+        {
+            if (i > 0)
+            {
                 regionItems[i].deleteButton.gameObject.SetActive(true);
-            } else regionItems[i].deleteButton.gameObject.SetActive(false);
+            }
+            else regionItems[i].deleteButton.gameObject.SetActive(false);
         }
     }
 }
--- Assets/Scripts/core/Car.cs
+++ Assets/Scripts/core/Car.cs
@@ -7,17 +7,25 @@ using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using UnityEngine;
 
-public class Car {
+public class Car
+{
     public string id;
-    private int year, maxLuggage, maxSeats;
-    private string name, color, brand, carPictureUrl;
+    private int year,
+    maxLuggage,
+    maxSeats;
+    private string name,
+    color,
+    brand,
+    carPictureUrl;
     private Texture2D picture;
     private string pictureBase64;
     private DateTime updated;
-    public Car(string id) {
+    public Car(string id)
+    {
         this.Id = id;
     }
-    public Car(string name, int year, int maxLuggage, int maxSeats, string brand, string color, Texture2D picture) {
+    public Car(string name, int year, int maxLuggage, int maxSeats, string brand, string color, Texture2D picture)
+    {
 
         this.Year = year;
         this.MaxLuggage = maxLuggage;
@@ -27,7 +35,8 @@ public class Car {
         this.Color = color;
         this.Picture = picture;
     }
-    public Car(string id, string name, int year, int maxLuggage, int maxSeats, string brand, string color, string carPictureUrl) {
+    public Car(string id, string name, int year, int maxLuggage, int maxSeats, string brand, string color, string carPictureUrl)
+    {
         this.Id = id;
         this.Year = year;
         this.MaxLuggage = maxLuggage;
@@ -37,7 +46,8 @@ public class Car {
         this.Color = color;
         this.CarPictureUrl = carPictureUrl;
     }
-    public Car(string id, string name, int year, int maxLuggage, int maxSeats, string brand, string color, Texture2D picture) {
+    public Car(string id, string name, int year, int maxLuggage, int maxSeats, string brand, string color, Texture2D picture)
+    {
         this.id = id;
         this.Year = year;
         this.MaxLuggage = maxLuggage;
@@ -47,7 +57,8 @@ public class Car {
         this.Color = color;
         this.Picture = picture;
     }
-    public JObject ToJson() {
+    public JObject ToJson()
+    {
         JObject carJ = new JObject();
         carJ[nameof(this.id)] = this.id;
         carJ[nameof(this.year)] = this.year;
@@ -60,90 +71,134 @@ public class Car {
         return carJ;
     }
 
-    public static Car ToObject(JObject json) {
+    public static Car ToObject(JObject json)
+    {
         string id = "";
         var oId = json["objectId"];
-        if (oId != null)
-            id = oId.ToString();
+        if (oId != null) id = oId.ToString();
         string name = "";
         var oName = json["name"];
-        if (oName != null)
-            name = oName.ToString();
+        if (oName != null) name = oName.ToString();
         int year = -1;
         var oYear = json[nameof(Car.year)];
-        if (oYear != null)
-            int.TryParse(oYear.ToString(), out year);
+        if (oYear != null) int.TryParse(oYear.ToString(), out year);
         int maxLuggage = -1;
         var mL = json[nameof(Car.maxLuggage)];
-        if (mL != null)
-            int.TryParse(mL.ToString(), out maxLuggage);
+        if (mL != null) int.TryParse(mL.ToString(), out maxLuggage);
         int maxSeats = -1;
         var mS = json[nameof(Car.maxSeats)];
-        if (mS != null)
-            int.TryParse(mS.ToString(), out maxSeats);
+        if (mS != null) int.TryParse(mS.ToString(), out maxSeats);
         string brand = "";
         var oBrand = json["brand"];
-        if (oBrand != null)
-            brand = oBrand.ToString();
+        if (oBrand != null) brand = oBrand.ToString();
         string color = "";
         var oColor = json["color"];
-        if (oColor != null)
-            color = oColor.ToString();
+        if (oColor != null) color = oColor.ToString();
         string carPictureUrl = json["picture"].ToString();
         return new Car(id, name, year, maxLuggage, maxSeats, brand, color, carPictureUrl);
     }
-    
-    public static string Validate(Car car) {
-        if (car.Year > DateTime.Now.Year || car.Year < 1900)
-            return "Please enter a valid car year";
-        if (car.MaxLuggage < 0 || car.MaxLuggage > 10) {
+
+    public static string Validate(Car car)
+    {
+        if (car.Year > DateTime.Now.Year || car.Year < 1900) return "Please enter a valid car year";
+        if (car.MaxLuggage < 0 || car.MaxLuggage > 10)
+        {
             return "Max luggage must be less or equal 10";
         }
-        if (car.MaxSeats < 0 || car.MaxSeats > 50) {
+        if (car.MaxSeats < 0 || car.MaxSeats > 50)
+        {
             return "Max seats must be less or equal 50";
         }
-        if (string.IsNullOrEmpty(car.Name) || car.Name.Length < 2) {
+        if (string.IsNullOrEmpty(car.Name) || car.Name.Length < 2)
+        {
             return "Invalid car name";
         }
         var regexItem = new Regex("^[a-zA-Z0-9 ]*$");
-        if (string.IsNullOrEmpty(car.Brand) || car.Brand.Length < 2 || car.Brand.Length > 15 || regexItem.IsMatch(car.Brand)) {
+        if (string.IsNullOrEmpty(car.Brand) || car.Brand.Length < 2 || car.Brand.Length > 15 || regexItem.IsMatch(car.Brand))
+        {
             return "Invalid car brand";
         }
         regexItem = new Regex("/^#[0-9A-F]{6}$/i.test('#AABBCC')");
-        if (regexItem.IsMatch(car.Color)) {
+        if (regexItem.IsMatch(car.Color))
+        {
             return "Invalid color";
         }
         /*if (string.IsNullOrEmpty(car.PictureBase64)) {
-            return "Please enter your car picture";
-        }*/
+                return "Please enter your car picture";
+            }*/
         return string.Empty;
     }
 
-    public string Id { get => id; set => id = value; }
-    public int Year { get => year; set => year = value; }
-    public int MaxLuggage { get => maxLuggage; set => maxLuggage = value; }
-    public int MaxSeats { get => maxSeats; set => maxSeats = value; }
-    public string Brand { get => brand; set => brand = value; }
-    public string Name { get => name; set => name = value; }
-    public string Color { get => color; set => color = value; }
-    public Texture2D Picture { get => picture; set { picture = value; if (value != null) { pictureBase64 = Convert.ToBase64String(value.EncodeToPNG()); } } }
-    public string PictureBase64 { get => pictureBase64; }
-    public DateTime Updated { get => updated; set => updated = value; }
-    public string CarPictureUrl { get => carPictureUrl; set => carPictureUrl = value; }
-
-    public override bool Equals(object obj) {
-        var car = obj as Car;
-        return car != null &&
-               id == car.id &&
-               year == car.year &&
-               maxLuggage == car.maxLuggage &&
-               maxSeats == car.maxSeats &&
-               name == car.name &&
-               color == car.color &&
-               brand == car.brand;
+    public string Id
+    {
+        get => id;
+        set => id = value;
+    }
+    public int Year
+    {
+        get => year;
+        set => year = value;
+    }
+    public int MaxLuggage
+    {
+        get => maxLuggage;
+        set => maxLuggage = value;
+    }
+    public int MaxSeats
+    {
+        get => maxSeats;
+        set => maxSeats = value;
+    }
+    public string Brand
+    {
+        get => brand;
+        set => brand = value;
+    }
+    public string Name
+    {
+        get => name;
+        set => name = value;
+    }
+    public string Color
+    {
+        get => color;
+        set => color = value;
+    }
+    public Texture2D Picture
+    {
+        get => picture;
+        set
+        {
+            picture = value;
+            if (value != null)
+            {
+                pictureBase64 = Convert.ToBase64String(value.EncodeToPNG());
+            }
+        }
+    }
+    public string PictureBase64
+    {
+        get => pictureBase64;
+    }
+    public DateTime Updated
+    {
+        get => updated;
+        set => updated = value;
+    }
+    public string CarPictureUrl
+    {
+        get => carPictureUrl;
+        set => carPictureUrl = value;
     }
 
-    public override string ToString() {
+    public override bool Equals(object obj)
+    {
+        var car = obj as Car;
+        return car != null && id == car.id && year == car.year && maxLuggage == car.maxLuggage && maxSeats == car.maxSeats && name == car.name && color == car.color && brand == car.brand;
+    }
+
+    public override string ToString()
+    {
         return "nice one";
     }
 
